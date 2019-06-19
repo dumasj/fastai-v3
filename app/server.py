90755016630c871d877d5fb98b2de8eb2d3c1945
Julie Dumas <dumasjk@gmail.com>
2019-06-19 12:24:53 -0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -61,8 +61,8 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    #acc = max(learn.predict(img)[2])
-    return JSONResponse({ 'result': str(prediction) })
+    acc = max(learn.predict(img)[2])
+    return JSONResponse({ 'result': str(acc) })
 
 
 if __name__ == '__main__':
