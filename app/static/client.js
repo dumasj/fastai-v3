6ec2a2e93c1a8d3c997a961c883cf07dadac3ca0
Julie Dumas <dumasjk@gmail.com>
2019-06-19 18:22:59 -0700

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -29,7 +29,8 @@ function analyze() {
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `Result = ${response["result"]}`;
+      var response2 = JSON.parse(e.target.responseText);
+      el("result-label").innerHTML = `Result = ${response["result"]} Prob = ${response2["acc"]}`;
     }
     el("analyze-button").innerHTML = "Analyze";
   };
